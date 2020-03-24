5f42905ec728470bb536729d432d47d669a420e3
moris <moris.rafol@gmail.com>
2020-03-24 15:31:04 +0200

    b: main.js - loaded the photos f: general.css - minor tweaks

--- script/main.js
+++ script/main.js
@@ -10,7 +10,7 @@ var articlesApi ={
 };
 var elements_articlesContainer = document.getElementById("articlesContainer");
 
-function fetchData(url,funcToApply) {
+function fetchJsonData(url, funcToApply) {
     fetch(url)
         .then(url=> url.json())
         .then(jsonObj=> {
@@ -18,30 +18,38 @@ function fetchData(url,funcToApply) {
         })
         .catch(e=> console.error(e));
 }
+function fetchAndLoadImg(url, imgElement) {
+    fetch(url)
+        .then(response=> response.blob())
+        .then(blobRes=> imgElement.src = URL.createObjectURL(blobRes));
+}
 
 
+function generateArticleDiv(title,content,imgURL) {
 
-function generateArticleDiv(title,content,imgSrc) {
     let divContainer = document.createElement("div");
     let titleElement = document.createElement("h3");
     let contentElement = document.createElement("p");
+    let imgElement = document.createElement("img")
 
+     // fetchAndLoadImg(imgURL, imgElement );
     titleElement.textContent = title;
     contentElement.textContent = content;
+    imgElement.src = imgURL;
 
+    divContainer.appendChild(imgElement);
     divContainer.appendChild(titleElement);
     divContainer.appendChild(contentElement);
     divContainer.classList.add("articleBody");
-
     return divContainer;
 }
 
 
-fetchData(articlesApi.getUrl("coronavirus"),jsonObj=>
+fetchJsonData(articlesApi.getUrl("coronavirus"), jsonObj=>
 {
     jsonObj.articles.forEach(article=>
     {
-        let newElement = generateArticleDiv(article.title,article.description);
+        let newElement = generateArticleDiv(article.title,article.description,article.urlToImage);
         elements_articlesContainer.appendChild(newElement);
     });
 
