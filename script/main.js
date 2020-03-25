1aafa0e0d3b3d4287eafc20b4bf3fa41012963d7
moris <moris.rafol@gmail.com>
2020-03-25 21:49:16 +0200

    f: main.js - implemented the search capability f: main.js - added suggestions to the search bar using an api f: main.js - organized the code
    r: added searchBar.css
    r: added img-icon-search.svg

--- script/main.js
+++ script/main.js
@@ -1,12 +1,18 @@
 
 
+//variables-----------------------------------------------------------------------------------------------------------------------------
+
+//articles-------------------------------------------------------------------------
 var articlesApi ={
     url: "https://newsapi.org/v2",
     apiKey : "9de2a3c2532845628d72a2c8e8d26c15",
     getUrl: function (searchQ) {
-        return `${articlesApi.url}/everything?q=${searchQ}&from=2020-03-23&to=2020-03-23&apiKey=${articlesApi.apiKey} +`
+        return `${articlesApi.url}/everything?q=${searchQ}&sortBy=publishedAt&language=en&apiKey=${articlesApi.apiKey} +`
     }
 };
+var elements_articlesContainer = document.getElementById("articlesContainer");
+let default_article_topic ="corona COVID-19";
+//weather--------------------------------------------------------------------------
 var weatherAPI = {
     proxyurl2: "https://agile-waters-87216.herokuapp.com/",
     url: "http://api.weatherstack.com/",
@@ -19,7 +25,6 @@ var weatherAPI = {
 
 var elements_weatherContainer = document.getElementById("weatherContainer");
 var elements_weatherPanel = document.getElementById("weatherpanel")
-var elements_articlesContainer = document.getElementById("articlesContainer");
 var elements_weathericon = document.getElementById("weathericon");
 var elements_currentTemp = document.getElementById("currentTemp");
 var elements_feelslike = document.getElementById("feelslike");
@@ -27,6 +32,21 @@ var elements_humidity = document.getElementById("humidity");
 var elements_wind = document.getElementById("wind");
 var elements_precipitation = document.getElementById("precipitation");
 
+
+//search=-------------------------------------------------------------------------
+let searchAPI = {
+    url: "https://api.cognitive.microsoft.com/bing/v7.0/suggestions",
+    apiKey : "93247677e13a4c3f97bba1e28ba8bde0",
+
+    getUrl: function (searchQ) {
+        return `${searchAPI.url}?query=${searchQ}`
+    }
+};
+let element_searchInputField = document.getElementById("searchBarInput");
+let element_searchSuggestionsContainer = document.getElementById("searchSuggestions");
+
+
+//functions-----------------------------------------------------------------------------------------------------------------------------
 function fetchJsonData(url, funcToApply) {
     fetch(url)
         .then(url=> url.json())
@@ -37,6 +57,7 @@ function fetchJsonData(url, funcToApply) {
         .catch(e=> console.error(e));
 }
 
+//articles-------------------------------------------------------------------------
 
 function generateArticleDiv(article) {
 
@@ -115,17 +136,23 @@ function generateDateString(jsonDateFormat){
 
 }
 
-fetchJsonData(articlesApi.getUrl("coronavirus"), jsonObj=>
-{
-    jsonObj.articles.forEach(article=>
+
+function loadArticles(searchStr){
+    fetchJsonData(articlesApi.getUrl(searchStr), jsonObj=>
     {
-        let newElement = generateArticleDiv(article);
-        elements_articlesContainer.appendChild(newElement);
+        elements_articlesContainer.innerHTML = "";
+        jsonObj.articles.forEach(article=>
+        {
+            let newElement = generateArticleDiv(article);
+            elements_articlesContainer.appendChild(newElement);
+        });
+
+
     });
+}
+loadArticles(default_article_topic);
 
-
-});
-
+//weather--------------------------------------------------------------------------
 fetchJsonData(weatherAPI.getUrl("Haifa"), jsonObj=>{
     console.log(jsonObj)
     elements_currentTemp.textContent += jsonObj.current.temperature + "°C";
@@ -135,8 +162,70 @@ fetchJsonData(weatherAPI.getUrl("Haifa"), jsonObj=>{
     elements_precipitation.textContent += jsonObj.current.precip + "mm";
     elements_weathericon.style.backgroundImage = `url(${jsonObj.current.weather_icons[0]})`
 })
-
 elements_weatherContainer.addEventListener("click", ()=>{
     elements_weatherPanel.classList.toggle("show");
     elements_weatherPanel.classList.toggle("active")
 })
+
+
+//search=-------------------------------------------------------------------------
+
+element_searchInputField.addEventListener("input",onInputChange);
+function onInputChange() {
+    let str = element_searchInputField.value;
+    if(!str || str.trim() === "")
+        str = default_article_topic;
+
+    // let currentTime = Date.now();
+    if(intervalID ) {
+        clearInterval(intervalID);
+        console.log("cleared")
+    }
+    intervalID= setInterval(()=>{
+
+        getSuggestions(str);
+        loadArticles(str);
+
+        let temp =intervalID;
+        intervalID = null;
+        clearInterval(temp);
+
+    },600);
+
+
+}
+
+let intervalID ;
+function getSuggestions(str){
+
+    fetchSuggestionData(searchAPI.getUrl(str),jsonObj=> {
+            // text = `{result:${text}}`;
+                if(!jsonObj.suggestionGroups || jsonObj.suggestionGroups.length === 0)
+                {
+                    element_searchSuggestionsContainer.innerHTML="";
+                    return;
+                }
+                jsonObj.suggestionGroups[0].searchSuggestions.forEach(suggestion => {
+                    let option = document.createElement("option");
+                    option.setAttribute("value", suggestion.displayText);
+                    element_searchSuggestionsContainer.appendChild(option)
+                });
+        }
+    )
+}
+function fetchSuggestionData(url , funcToApply) {
+
+    const myRequest = new Request(url, {
+        method: 'GET',
+        headers: {"Ocp-Apim-Subscription-Key": "93247677e13a4c3f97bba1e28ba8bde0"},
+    });
+
+    fetch(myRequest)
+        .then(response=> response.json())
+        .then(Obj=> {
+            console.log(Obj);
+            funcToApply(Obj);
+        })
+        .catch(e=> console.error(e));
+}
+
